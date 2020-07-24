3c2bae1cdbc7ceffd45e3b5c91493e7902475453
Joss Doebler <mail@joss-doebler.de>
2020-07-24 18:44:34 +0200

    Darkmode styles for ConfirmOverlay

--- src/components/confirmoverlay/ConfirmOverlay.js
+++ src/components/confirmoverlay/ConfirmOverlay.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { motion } from 'framer-motion';
+
+import { DarkmodeContext } from 'context';
 
 import styles from './ConfirmOverlay.module.css';
 import { fadeIn } from 'animations';
 
 function ConfirmOverlay({ toggleOverlay, clearList }) {
+  const { isDarkmode } = useContext(DarkmodeContext);
+
   const handleClearButton = () => {
     clearList();
+    toggleOverlay();
+  };
+
+  const handleCancelButton = () => {
     toggleOverlay();
   };
 
   return (
     <>
       <motion.div className={styles.background} {...fadeIn} />
-      <motion.div className={styles.confirmdialog} {...fadeIn}>
+      <motion.div
+        className={`${styles.confirmdialog} ${isDarkmode && styles.dark}`}
+        {...fadeIn}
+      >
         <p className={styles.confirmdialogp}>Are you sure?</p>
-        <button
-          className={styles.confirmdelete}
-          onClick={() => handleClearButton()}
-        >
+        <button className={styles.confirmdelete} onClick={handleClearButton}>
           Clear
         </button>
-        <button className={styles.canceldelete} onClick={() => toggleOverlay()}>
+        <button className={styles.canceldelete} onClick={handleCancelButton}>
           Cancel
         </button>
       </motion.div>
