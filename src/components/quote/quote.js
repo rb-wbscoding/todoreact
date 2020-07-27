b5007387521c6713a829c93a64c0128b3b04040f
Joss Doebler <mail@joss-doebler.de>
2020-07-27 13:24:58 +0200

    Removed unnecessary code and replaced default text in html

--- src/components/quote/quote.js
+++ src/components/quote/quote.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef, useContext } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import { DarkmodeContext } from 'context';
 
