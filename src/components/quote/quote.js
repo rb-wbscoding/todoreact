251e388f2dc6027779781d26293d787db3adc551
richard boucher <rb.wbscoding@gmail.com>
2020-07-27 15:19:01 +0200

    simplified quote

--- src/components/quote/quote.js
+++ src/components/quote/quote.js
@@ -1,11 +1,10 @@
-import React, {useState, useEffect, useRef} from "react"
+import React, {useState, useEffect} from "react"
 import styles from "./quote.module.css";
 
 
 
 function Quote() {
 
-const [quote, setQuotes] = useState([]);
 const [singlequote, setSinglequote]=useState({quotes: "", authors: ""})
 
 useEffect(()=>{    fetch("https://type.fit/api/quotes")
@@ -15,26 +14,19 @@ useEffect(()=>{    fetch("https://type.fit/api/quotes")
         const randomnumber = Math.floor(Math.random()*1642)
         const textquote=data[randomnumber].text;
         const authorquote=data[randomnumber].author;
-        setQuotes(data);
         setSinglequote({quotes: textquote, authors: authorquote});
-        //console.log(textquote)
+        setInterval(
+        function getonequote(){
+        const randomnumber = Math.floor(Math.random()*1642);
+        const textquote2=data[randomnumber].text;
+        const authorquote2=data[randomnumber].author;
+        setSinglequote({quotes: textquote2, authors: authorquote2});
+    }, 10000);
     })
     .catch(error => {
         console.log("something bad happened somewhere, rollback!");
     })}, [])
     
-    const countRef=useRef(quote)
-    countRef.current=quote;
-    //console.log(quote)
-useEffect(() =>{
-    setInterval(
-        function getonequote(){
-        const randomnumber = Math.floor(Math.random()*1642);
-        const textquote2=countRef.current[randomnumber].text;
-        const authorquote2=countRef.current[randomnumber].author;
-        setSinglequote({quotes: textquote2, authors: authorquote2});
-    }, 10000);
-}, [])
     
     return ( 
             <div className={styles.container}>
